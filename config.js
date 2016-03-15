b15ca2d684ce23c67a7db6a8c1b35523881879d4
Mithun Das <mithun.programmer@gmail.com>
2016-03-15 15:43:04 +0530

    remove files

--- config.js
+++
@@ -1,5 +0,0 @@
-window.GoogleSamples = window.GoogleSamples || {};
-window.GoogleSamples.Config = window.GoogleSamples.Config || {
-    gcmAPIKey: 'AIzaSyB3OTx_UylyI8nYGRxSjVC0oz70CsaKQbY'
-//    pushServerURL: 'http://localhost:8000/'
-};
